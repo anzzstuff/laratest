53b0a5e42d85f8a158264cdfb05aaaf48be3efc7
AnttiP <junjunjunn@gmail.com>
2016-05-18 17:41:37 +0000

    ep10

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,10 +2,12 @@
 <html>
     <head>
         <title>Test</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <?php // <link rel="stylesheet" href="{{ elixir('css/app.css') }}"> ?>
     </head>
     <body>
-        
-        @yield('content')
-        <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
